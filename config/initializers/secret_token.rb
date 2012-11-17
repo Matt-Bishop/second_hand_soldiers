a0eb493e28db57001892e5d16913d5e420a2a27c
Matt Bishop <mattheweugenebishop@gmail.com>
2012-11-16 19:27:11 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondHandSoldiers::Application.config.secret_token = '58d5bb3116049fc8fb3ea4100ef5005c80c922679c3a2f36b468e6314a0967ab6172bc170792a3b1e82b2f9e0e57c7084608b4be7bfbd8bd3887918e4feaa6ba'
